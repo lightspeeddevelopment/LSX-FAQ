0acdec90d80984151c67829d613e48f7555aed6e
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-10-21 17:41:26 -0300

    testing faqs triggers

--- assets/js/lsx-faq.min.js
+++ assets/js/lsx-faq.min.js
@@ -2,14 +2,18 @@ jQuery(document).ready(function( ) {
     jQuery(document).on('facetwp-loaded', function() {
 
         jQuery('.faq li .question').on( 'click', function() {
+            console.log('testing1');
             jQuery(this).find('.plus-minus-toggle').toggleClass('collapsed');
+            console.log('testing2');
             jQuery(this).parents( 'li' ).toggleClass('active');
         });
     });
 
     if ( jQuery( 'body' ).hasClass( 'single-product' ) ) {
         jQuery('.faq li .question').on( 'click', function() {
+            console.log('testing3');
             jQuery(this).find('.plus-minus-toggle').toggleClass('collapsed');
+            console.log('testing4');
             jQuery(this).parents( 'li' ).toggleClass('active');
         });
     }
